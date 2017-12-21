d6c072aa6e2221cf5ecb400231e4fc8e66437ca4
Bartolo96 <bartolocorcella@gmail.com>
2017-12-21 17:29:14 +0100

    Add files via upload

--- recaptcha_helper.php
+++ recaptcha_helper.php
@@ -38,7 +38,7 @@
 define("RECAPTCHA_API_SERVER", "http://www.google.com/recaptcha/api");
 define("RECAPTCHA_API_SECURE_SERVER", "https://www.google.com/recaptcha/api");
 define("RECAPTCHA_VERIFY_SERVER", "www.google.com");
-include_once 'csrfprotector.php';
+require 'csrfprotector.php';
 
 //Initialise CSRFGuard library
 csrfProtector::init();
